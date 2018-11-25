abc079b9a3ee7ad35891389b0bf300ab12e62480
Akshata Basrur <akshatabasrur@gmail.com>
2018-11-24 23:45:46 -0500

    commit myReactApplication

--- src/Nav.js
+++ src/Nav.js
@@ -4,32 +4,16 @@ import m1 from './m1.jpg';
 import m2 from './m2.jpg';
 
 class Nav extends Component {
-    constructor(){
-        super();
-        this.state={
-            text: "m2"
-        };
-    }
-
-    clicked(text)
-    {
-        //this.setState({ text: text});
-        this.forceUpdate();
-    }
-  render() {
+      render() {
     return (
     	<div class="nav">
+        <center>
 		<h2>Explore more of .. </h2>
-         <a href="https://guidetoiceland.is/travel-iceland/drive/seljalandsfoss"><li>Seljalandsfoss, Iceland</li></a>
-         <a href="https://www.welcomeargentina.com/puertoiguazu/iguazu-falls.html"><li> Iguazu Falls, Argentina</li></a>
-         <a href="https://visitgreenland.com/destinations/nuuk/"> <li> Nuuk, Greenland</li></a>
-         
-         <h2>Explore more of .. </h2>
-         
-        {this.state.text}
-        
-
-	    </div>
+         <a href="https://guidetoiceland.is/travel-iceland/drive/seljalandsfoss"><li>Seljalandsfoss, Iceland</li></a><br></br>
+         <a href="https://www.welcomeargentina.com/puertoiguazu/iguazu-falls.html"><li> Iguazu Falls, Argentina</li></a><br/>
+         <a href="https://visitgreenland.com/destinations/nuuk/"> <li>Nuuk, Greenland</li></a>
+         </center>
+         </div>
     );
   }
 }
